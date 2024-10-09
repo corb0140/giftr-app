117df540e03eb226b0e8785feb0f45576c71a569
Mark Corbin <markpc1608@gmail.com>
2024-10-09 00:28:45 -0400

    added styling to people screen

--- screens/PeopleScreen.js
+++ screens/PeopleScreen.js
@@ -31,7 +31,7 @@ export default function PeopleScreen() {
         deletePerson(id);
       }}
     >
-      <FontAwesome6 name="trash" size={30} color="white" />
+      <FontAwesome6 name="trash" size={24} color="white" />
     </Pressable>
   );
 
@@ -69,31 +69,22 @@ export default function PeopleScreen() {
           {people.length === 0 ? (
             <View>
               <Text>No People Added</Text>
-
-              <Pressable
-                style={styles.addPersonButton}
-                title="Add Person"
-                onPress={() => navigation.navigate("AddPerson")}
-              >
-                <Text style={{ color: "white" }}>Add Person</Text>
-              </Pressable>
             </View>
           ) : (
-            <>
-              <FlatList
-                data={people.sort((a, b) => a.dob.localeCompare(b.dob))}
-                keyExtractor={(item) => item.id}
-                renderItem={renderItem}
-              />
-              <Pressable
-                style={styles.addPersonButton}
-                title="Add Person"
-                onPress={() => navigation.navigate("AddPerson")}
-              >
-                <Text style={{ color: "white" }}>Add Person</Text>
-              </Pressable>
-            </>
+            <FlatList
+              data={people.sort((a, b) => a.dob.localeCompare(b.dob))}
+              keyExtractor={(item) => item.id}
+              renderItem={renderItem}
+            />
           )}
+
+          <Pressable
+            style={styles.addPersonButton}
+            title="Add Person"
+            onPress={() => navigation.navigate("AddPerson")}
+          >
+            <FontAwesome6 name="plus" size={24} color="white" />
+          </Pressable>
 
           {people.length === 0 && (
             <ModalComponent
@@ -115,28 +106,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
-  peopleListView: {
-    marginVertical: 20,
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  peopleListMessageView: {
-    borderBottomWidth: 2,
-    borderBottomColor: "#f0f0f0",
-  },
-  peopleListMessage: {
-    fontSize: 22,
-    color: "#a0a0a0",
-    paddingBottom: 10,
-  },
   itemContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    backgroundColor: "#f0f0f0",
-    borderBottomWidth: 2,
-    borderBlockColor: "#d0d0d0",
-    padding: 10,
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 20,
+    marginHorizontal: 10,
+    marginVertical: 10,
+    elevation: 5,
+    shadowColor: "#343434",
+    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 2,
   },
   itemInfo: {
     flexDirection: "column",
@@ -145,6 +129,7 @@ const styles = StyleSheet.create({
   itemName: {
     fontSize: 25,
     fontWeight: "bold",
+    color: "deepskyblue",
   },
   itemDate: {
     fontSize: 18,
@@ -156,11 +141,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#d10000",
     width: 80,
+    borderRadius: 10,
+    marginVertical: 10,
+    marginRight: 5,
   },
   addPersonButton: {
+    position: "absolute",
+    bottom: 30,
+    right: "50%",
+    transform: "translateX(25px)",
+    borderRadius: 100,
     backgroundColor: "deepskyblue",
-    padding: 15,
     alignItems: "center",
-    margin: 10,
+    justifyContent: "center",
+    height: 50,
+    width: 50,
   },
 });
